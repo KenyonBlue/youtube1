f9e48ee24a679a9a5e4f33aaf010aff442e64079
Kenyon Blue <bluekenyfleo@gmail.com>
2019-04-27 14:53:54 -0400

    added react vector icons, addded nav bar and icons

--- App.js
+++ App.js
@@ -1,17 +1,30 @@
 
 
 import React, {Component} from 'react';
-import { StyleSheet, Text, View, Image} from 'react-native';
-
+import { StyleSheet, Text, View, Image, TouchableOpacity} from 'react-native';
+// this is a package from react-native-vector-icons on github
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-       <View style={styles.navBar}>
-       <Image source={require('./components/images/youtube-logo-png-photo-0.png')} style={{width: 98, height: 22}}/>
-       </View>
+          <View style={styles.navBar}>
+          <Image source={require('./components/images/youtube-logo-png-photo-0.png')} style={{width: 98, height: 22}}/>
+            <View style={styles.rightNav}>
+              <TouchableOpacity>
+                <Icon style={styles.navItem} name="search" size={25}/>
+              </TouchableOpacity>
+              <TouchableOpacity>
+                <Icon  style={styles.navItem} name="account-circle" size={25}/>
+              </TouchableOpacity>
+            </View>
+          </View>
+          <View style={styles.body}>
+            </View>
+          <View style={styles.tabBar}>
+          </View>
       </View>
     );
   }
@@ -29,5 +42,21 @@ const styles = StyleSheet.create({
     paddingHorizontal: 15,
     flexDirection: 'row',
     alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  rightNav: {
+    flexDirection: 'row',
+  },
+  navItem: {
+    marginLeft: 25,
+
+  },
+  tabBar: {
+    backgroundColor: 'red',
+    height: 60,
+    
+  },
+  body: {
+    flex: 1
   }
 });
